04903742a0e6df659407bcef6135d83b0ead37d1
Nick Havens <npahavens@gmail.com>
2017-07-08 11:07:41 -0500

    completed onListItemHover method

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 // TODO
 class GroceryListItem extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
   render() {
-    return <li>{this.props.item}</li>;
+    var style  = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
+    return <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.item}</li>;
   }
 }
 
 var GroceryList = (props) => {
-    return <ul>{
-      props.items.map((item, i) => {
-        return <GroceryListItem item={item} key={i}/>
-      })
-    }</ul>
+  return <ul>{
+    props.items.map((item, i) => {
+      return <GroceryListItem item={item} key={i}/>
+    })
+  }</ul>
 }
 
 
